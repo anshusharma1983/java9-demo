3a0a933541399a770ebf7e775b5585b60bcffdf5
Anshu <anshusha@amazon.com>
2017-07-10 19:42:13 +0530

    jlink primesapp

--- build.sh
+++ build.sh
@@ -1,7 +1,10 @@
 rm -rf mods
+rm -rf primesapp
 
 mkdir -p mods
 
 javac -d mods --module-source-path src `find src -type f`
 
-java --module-path mods -m amazon.primes/amazon.primes.PrimeService
+jlink --module-path $JAVA_HOME/jmods:mods --add-modules amazon.primes --output primesapp
+
+./primesapp/bin/java -m amazon.primes/amazon.primes.PrimeService
